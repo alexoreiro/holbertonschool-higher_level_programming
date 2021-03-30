5ff245fe685729aa86001844eac034a2af33ec31
alexoreiro <alexis.oreiro@icloud.com>
2021-03-30 16:02:22 +0000

    fixed task

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+from magic_calculation_102 import add, sub
+
+
 def magic_calculation(a, b):
-    return 98 + a ** b
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
