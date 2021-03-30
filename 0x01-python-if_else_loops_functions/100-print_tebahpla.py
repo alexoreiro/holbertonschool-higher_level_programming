6413f1b8fdf83a868e67937b33e28b40485fd330
alexoreiro <alexis.oreiro@icloud.com>
2021-03-30 15:39:02 +0000

    file changes

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for c in range(ord('z'), ord('a') - 1, -1):
-    if c % 2 == 0:
-        print("{:s}".format(chr(c)), end="")
+i = 122
+for j in range(26):
+    print("{}".format(chr(i)), end="")
+    if (i > 96):
+        i -= 33
     else:
-        print("{:s}".format(chr(c - 32)), end="")
+        i += 31
