c054c204770ff813407948a1ca0747ff04758ce3
alexoreiro <alexis.oreiro@icloud.com>
2021-04-15 19:52:04 +0000

    change file

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-"""
-MyList - subclass of list
-"""
 
+# Print a sorted function
 
 class MyList(list):
-    """ public method: print sorted list """
     def print_sorted(self):
-        """ sorts list and then print """
-        new_list = self[:]
-        new_list.sort()
-        print(new_list)
+        print(sorted(self))
