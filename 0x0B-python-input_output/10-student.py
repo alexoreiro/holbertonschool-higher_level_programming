85017b0f8c4bcd1fb66c9cded3a58b3e3d4cf830
alexoreiro <alexis.oreiro@hotmail.com>
2021-04-23 23:47:09 +0000

    student

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
-"""
-Class Student
-"""
 
+# Class Student
 
 class Student:
-    """ Defines class Student """
+    # Defines class Student 
     def __init__(self, first_name, last_name, age):
-        """ initiates attributes for student instance """
+        # initiates attributes for student instance 
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """ retrieves a dictionary representation of a student instance """
-        return self.__dict__
+    def to_json(self, attrs=None):
+        # json function
+        if attrs is not None:
+            new_dict = {}
+            for obj in attrs:
+                if type(obj) != str:
+                    return self.__dict__
+            for obj in attrs:
+                if obj in self.__dict__:
+                    new_dict[obj] = self.__dict__[obj]
+            return new_dict
+        else:
+            return self.__dict__
