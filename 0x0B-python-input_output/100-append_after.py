a59b6eff1ab9fe2b7e0ab9e78e072b7d51919f1d
alexoreiro <alexis.oreiro@hotmail.com>
2021-04-24 17:00:08 +0000

    search and update

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# Append After
+def append_after(filename="", search_string="", new_string=""):
+
+    # Inserts a line of text to a file
+    inputfile = open(filename, 'r').readlines()
+    with open(filename, 'w') as write_file:
+        for line in inputfile:
+            write_file.write(line)
+            if search_string in line:
+                write_file.write(new_string)
+        write_file.close()
