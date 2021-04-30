95acb1321a7e7a7eaeb2e49108f44865ee9d267c
alexoreiro <alexis.oreiro@hotmail.com>
2021-04-30 02:00:17 +0000

    add integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""
-Module with add_integer function
-"""
+
+# Module with add_integer function
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("test/0-add_integer.txt")
 
 
 def add_integer(a, b=98):
-    """
-    Returns sum of a and b
-    """
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-    if not isinstance(a, int):
+    
+    # Returns sum of a and b
+    
+    if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
-    if not isinstance(b, int):
+    elif type(b) != int and type(b) != float:
         raise TypeError("b must be an integer")
-    return a + b
+    else:
+        return int(a + b)
