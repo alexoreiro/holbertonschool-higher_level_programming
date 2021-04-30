87c7694e798dcc1abc5c27622d760a5ff8e7f316
alexoreiro <alexis.oreiro@hotmail.com>
2021-04-30 01:25:33 +0000

    average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,17 +1,4 @@
 #!/usr/bin/python3
-
 def weight_average(my_list=[]):
-    mul = []
-    final = []
-    multi = 1.0
-    if my_list:
-        for element in my_list:
-            for i in range(len(element)):
-                multi *= element[i]
-                if i + 1 == len(element):
-                    final.append(element[i])
-            mul.append(multi)
-            mul = 1
-        return float(sum(mul)) / float(sum(final))
-    else:
-        return 0
+    return sum([x[0] * x[1] for x in my_list]) / \
+        (sum([x[1] for x in my_list]) or 1)
