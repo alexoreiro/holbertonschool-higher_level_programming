d97491051a4a57c708684497188bb39e06a70e2b
alexoreiro <alexis.oreiro@hotmail.com>
2021-05-04 21:22:17 +0000

    prints the title of SW movie

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
-// makes get request for SW movie id
+
 const request = require('request');
-request('https://swapi-api.hbtn.io/api/films/'${process.argv[2]}, function (error, response, body) {
-  error && console.log(error);
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
   console.log(JSON.parse(body).title);
 });
