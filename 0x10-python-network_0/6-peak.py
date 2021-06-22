ff2edc8742dd5f3ed01d739bc7d408f6ea5a55dd
alexoreiro <alexis.oreiro@hotmail.com>
2021-06-22 15:41:06 +0000

    changes

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-"""
-Module containing find_peak in a list of unsorted integers
-"""
+'''  finds a peak in a list of unsorted integers. '''
 
 
 def find_peak(list_of_integers):
-    """ finds a peak in a list of unsorted integers """
+    ''' finds a peak in a list '''
     if not list_of_integers:
         return None
-    elif len(list_of_integers) == 1:
+    if len(list_of_integers) == 1:
         return list_of_integers[0]
-    peak = list_of_integers[0]
-    for x in range(len(list_of_integers) - 1):
-        if list_of_integers[x + 1] > list_of_integers[x]:
-            peak = list_of_integers[x + 1]
-        elif list_of_integers[x] > list_of_integers[x + 1]:
-            peak = list_of_integers[x]
-    return peak
+    if len(list_of_integers) == 2:
+        return max(list_of_integers)
+
+    mid_size = int(len(list_of_integers) / 2)
+    middle = list_of_integers[mid_size]
+    if middle > list_of_integers[mid_size - 1]\
+       and middle > list_of_integers[mid_size + 1]:
+        return middle
+    elif middle < list_of_integers[mid_size - 1]:
+        return find_peak(list_of_integers[:mid_size])
+    else:
+        return find_peak(list_of_integers[mid_size + 1:])
